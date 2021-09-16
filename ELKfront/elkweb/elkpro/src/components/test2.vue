2a4bedd4079bfe6d5011a9019da35d377e835574
DaPingKuoLuo <fengyxiii@163.com>
2021-09-16 21:08:25 +0800

    text2

--- ELKfront/elkweb/elkpro/src/components/test2.vue
+++ ELKfront/elkweb/elkpro/src/components/test2.vue
@@ -1,36 +1,5 @@
 <template>
-    <el-container>
-    <el-table
-            height="250"
-            ref="singleTable"
-            :data="tableData"
-            highlight-current-row
-            @current-change="handleCurrentChange"
-            style="width: 100%">
-        <el-table-column
-                type="index"
-                width="50">
-        </el-table-column>
-        <el-table-column
-                property="date"
-                label="日期"
-                width="120">
-        </el-table-column>
-        <el-table-column
-                property="name"
-                label="姓名"
-                width="120">
-        </el-table-column>
-        <el-table-column
-                property="address"
-                label="地址">
-        </el-table-column>
-    </el-table>
-    <div style="margin-top: 20px">
-        <el-button @click="setCurrent(tableData[1])">选中第二行</el-button>
-        <el-button @click="setCurrent()">取消选择</el-button>
-    </div>
-    </el-container>
+    <div>修改</div>
 </template>
 
 <script>
